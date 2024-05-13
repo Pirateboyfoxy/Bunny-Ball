e0dea2fd5f58657dc359904189947c855e1b6ee0
Pirateboyfoxy <146957835+Pirateboyfoxy@users.noreply.github.com>
2024-05-13 09:22:10 -0400

    Initial commit

---
+++ Assets/Scripts/Game Instructions.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstructions : MonoBehaviour
+{
+    private string message = "Use WASD to move. Navigate though the maze and find the key!";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
