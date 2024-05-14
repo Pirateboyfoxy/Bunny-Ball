d0d5281d5a40edd6444a17d2efd6b75b69d6f4bf
Pirateboyfoxy <146957835+Pirateboyfoxy@users.noreply.github.com>
2024-05-14 09:23:32 -0400

    Create s speed variable

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public Rigidbody rb;
     public Transform cameraTransform;
     public GameManager gameManager;
+    public int speed = 5;
 
     void Update()
     {
@@ -19,7 +20,7 @@ public class Player : MonoBehaviour
         forward.Normalize();
         right.Normalize();
         Vector3 direction = forward * moveVertical + right * moveHorizontal;
-        rb.AddForce(direction * 5);
+        rb.AddForce(direction * speed);
     }
 
 }
