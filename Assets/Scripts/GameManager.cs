2ee0e0c97b11bf558ba50e0a6ccd4de97f0527ec
Pirateboyfoxy <146957835+Pirateboyfoxy@users.noreply.github.com>
2024-05-20 09:23:18 -0400

    Switch Level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,18 @@ public class GameManager : MonoBehaviour
     public bool gameOver;
     public GameObject winnerUI;
     public GameObject player;
+    //Level switch code
+    public GameObject LevelOne;
+    public GameObject LevelTwo;
+    public int currentLevel = 1;
 
     private void Update()
     {
+        if(currentLevel == 2)
+        {
+            LevelOne.SetActive(false);
+            LevelTwo.SetActive(true);
+        }
        //is the game over?
        if(gameOver == true)
        {
